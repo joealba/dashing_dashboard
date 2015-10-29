adcf30f7972f86b94fffaae1ae9c56569d1ccdb2
Joe Alba <joealba@JoeBookPro.local>
2015-10-29 16:43:08 -0400

    Added twitter search in addition to timeline

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -18,13 +18,23 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     # search_term = URI::encode('#todayilearned')
     # tweets = twitter.search("#{search_term}")
 
+    search_term = URI::encode('rigov')
+    search_tweets = twitter.search("#{search_term}")
+
     tweets = twitter.home_timeline
 
     if tweets
       tweets = tweets.map do |tweet|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
-      send_event('twitter_mentions', comments: tweets)
+      send_event('twitter_timeline', comments: tweets)
+    end
+
+    if search_tweets
+      search_tweets = search_tweets.map do |tweet|
+        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
+      end
+      send_event('twitter_search', comments: search_tweets)
     end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
